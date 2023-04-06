4c21e52e0f90adaee4b4a12c4e15580750f7e5d9
dgichaba <dorothygichaba@gmail.com>
2023-04-06 08:31:45 +0300

    update files

--- js/form.js
+++ js/form.js
@@ -1,13 +1,13 @@
 // function to send email and return a message
-sendMessage()=>{
+function sendMessage(){
   var name =$("#name").val();
   var email=$("#email").val();
   var message=$("#message").val();
   if (name === "" || email==="" || message===""){
-    $("#response").html("");  
- }else{
-  $("#error").html("");
-  $("#response").focus();
-  $("#response").html("Thank you " +name+"! Your message has been received. We shall get back to you shorty");
- }
+    $("#response").html("");}
+  else{
+    $("#error").html("");
+    $("#response").focus();
+    $("#response").html("Thank you " +name+"! Your message has been received. We shall get back to you shorty");
+  }
 }
